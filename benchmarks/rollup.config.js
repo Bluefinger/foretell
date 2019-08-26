8773c01a3dce10965534f969b8f20f7898fa3bee
Bluefinger <bluefinger@gmail.com>
2019-08-26 19:09:00 +0200

    Security audit fix and travis update

--- benchmarks/rollup.config.js
+++ benchmarks/rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from "rollup-plugin-commonjs";
 import nodeResolve from "rollup-plugin-node-resolve";
-import staticSite from "rollup-plugin-static-site";
 import serve from "rollup-plugin-serve";
 
 export default {
@@ -11,17 +10,10 @@ export default {
       mainFields: ["browser", "module", "main"]
     }),
     commonjs(),
-    staticSite({
-      dir: "dist",
-      moreScripts: [
-        "https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js",
-        "https://cdn.jsdelivr.net/npm/benchmark@2.1.4/benchmark.min.js"
-      ]
-    }),
-    serve("dist")
+    serve("public")
   ],
   output: {
-    file: "./dist/test.js",
+    file: "./public/dist/test.js",
     format: "iife",
     name: "Bench",
     sourcemap: true,
